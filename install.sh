5e88b61862b8d2b9576db9b0538664ce1fdb8adf
Jahred Danker <jahrede@gmail.com>
2023-02-18 15:25:04 -0600

    refactored install script to fix any issues

--- install.sh
+++ install.sh
@@ -1,20 +1,18 @@
 ## Install script to setup dev environment
+yes | sudo apt-get install build-essential
+
+yes | sudo apt install zsh
+
+sudo chsh -s $(which zsh) $USER
 
 ## install homebrew (linuxbrew)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-sudo apt-get install build-essential
-
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
-# install powerlevel10k
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 ## linux brew to install packages
 brew install \
@@ -33,33 +31,25 @@ brew install \
 stow nvim
 stow git
 stow zsh
+stow p10k 
+
+# install powerlevel10k
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
 # add zsh to valid login shells
 command -v zsh | sudo tee -a /etc/shells
 
-# set zsh default shell
-sudo chsh -s $(which zsh) $USER
-
-
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# load existing configs for oh-my-zsh
+cat .zshrc.pre-oh-my-zsh >> .zshrc
 
 
 # install neovim plugins
 nvim --headless +PlugInstall +qall
 
-
 # run zsh 
-exec zsh
-
-
-
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-
-# install neovim plugins
-nvim --headless +PlugInstall +qall
-
-
-# run zsh 
-exec zsh
+zsh
