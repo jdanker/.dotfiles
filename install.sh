bf4c9f7e64cb7e05ef93809a16b7987439de75f9
Jahred Danker <jahrede@gmail.com>
2023-02-18 15:45:19 -0600

    update

--- install.sh
+++ install.sh
@@ -8,10 +8,8 @@ sudo chsh -s $(which zsh) $USER
 ## install homebrew (linuxbrew)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
-echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/jdanker/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 
 ## linux brew to install packages
